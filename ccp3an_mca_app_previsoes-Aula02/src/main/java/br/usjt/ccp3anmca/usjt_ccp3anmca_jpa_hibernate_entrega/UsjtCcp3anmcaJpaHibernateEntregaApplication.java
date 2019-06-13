c463d686d848ef3fdab5e652cfd5539a1db29d2e
Pablo Tamburu <36866579+ptamburu@users.noreply.github.com>
2019-06-13 13:08:33 -0300

    aula 02

---
+++ ccp3an_mca_app_previsoes-Aula02/src/main/java/br/usjt/ccp3anmca/usjt_ccp3anmca_jpa_hibernate_entrega/UsjtCcp3anmcaJpaHibernateEntregaApplication.java
@@ -0,0 +1,14 @@
+package br.usjt.ccp3anmca.usjt_ccp3anmca_jpa_hibernate_entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsjtCcp3anmcaJpaHibernateEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsjtCcp3anmcaJpaHibernateEntregaApplication.class, args);
+		
+	}
+
+}
---
+++ ccp3an_mca_app_previsoes-Aula02/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into previsao (id, dia_semana, temperatura_minima, temperatura_maxima, humidade_relativa, descricao, latitude, longitude) values (1,'Segunda-feira', 30.0, 40.0,20.0,'Sol com muitas nuvens. Pancadas de chuva à tarde e à noite', 1.345, 4.564);
+
+insert into previsao (id, dia_semana, temperatura_minima, temperatura_maxima, humidade_relativa, descricao, latitude, longitude) values (2,'Terça-feira', 19.0, 22.0,40.0,'Sol e aumento de nuvens de manhã. Pancadas de chuva à tarde e à noite.', 1.345, 4.564);
+
+insert into previsao (id, dia_semana, temperatura_minima, temperatura_maxima, humidade_relativa, descricao, latitude, longitude) values (3,'Quarta-feira', 17.0, 26.0,80.0,'Sol com muitas nuvens durante o dia. Períodos de nublado, com chuva a qualquer hora.', 1.345, 4.564);
+
+insert into previsao (id, dia_semana, temperatura_minima, temperatura_maxima, humidade_relativa, descricao, latitude, longitude) values (4,'Quinta-feira', 15.0, 30.0,90.0,'Chuvoso durante o dia e à noite.', 1.345, 4.564);
+
+insert into usuario (id, login, senha) values (1, 'admin','admin');
